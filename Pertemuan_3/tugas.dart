d27ca15cb7d45cc3484860238e6e3c128087bba1
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2025-09-10 18:48:44 +0700

    Tugas Praktikum_Bilangan Prima

---
+++ Pertemuan_3/tugas.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  for (int i = 0; i <= 201; i++) {
+    if (isPrima(i)) {
+      print("$i adalah bilangan prima. Nama: Mohammad Roihan Al Fahmi Hidayatulloh, NIM: 2341720097");
+    }
+  }
+}
+
+bool isPrima(int angka) {
+  if (angka < 2) return false;
+  for (int i = 2; i <= angka ~/ 2; i++) {
+    if (angka % i == 0) return false;
+  }
+  return true;
+}
